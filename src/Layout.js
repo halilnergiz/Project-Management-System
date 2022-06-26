d7d85ece5d89105a94f7e89278784b456b070e2a
DEONSKY <bestrasen@hotmail.com>
2022-06-26 16:09:00 +0300

    eslint implementation

--- src/Layout.js
+++ src/Layout.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import SideBar from './components/SideBar/SideBar'
-import styled from 'styled-components'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import SideBar from './components/SideBar/SideBar';
+import styled from 'styled-components';
 
 const LayoutWrapper = styled.div`
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: row;
-`
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+`;
 const MainLayout = styled.div`
-    height: 100%;
-    width: 97%;
-
-`
+  height: 100%;
+  width: 97%;
+`;
 
 const OutletWrapper = styled.div`
-    border: 1px solid black;
-    width: 90%;
-    float: right;
-    margin-top: 1rem;
-    margin-right: 1rem;
-`
+  border: 1px solid black;
+  width: 90%;
+  float: right;
+  margin-top: 1rem;
+  margin-right: 1rem;
+`;
 
 const Layout = () => {
   return (
@@ -31,20 +30,17 @@ const Layout = () => {
       <SideBar />
 
       <MainLayout>
-
         <Header />
 
         <OutletWrapper>
           <Outlet />
         </OutletWrapper>
-
       </MainLayout>
-
     </LayoutWrapper>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
 
 // outlet -> tıklandığında yansıtılacak componentleri temsil eder.
 // outlet'i nerede tanımlarsan yerine geçecek componentler de oraya yazdırılır.
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoMatch = () => {
   return (
     <div>
-
       NO PAGE
       <p>
         <Link to="/">Go to the home page</Link>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default NoMatch
+export default NoMatch;
